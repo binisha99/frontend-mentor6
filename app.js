5b8aef93cca7cbac7a0ba9287a998632d52904b2
binisha <binishabajracharya708@gmail.com>
2023-04-11 16:24:48 +0545

    sub

--- app.js
+++ app.js
@@ -6,5 +6,40 @@ document.querySelectorAll('.content__question').forEach((item) => {
 		if (!item.classList.contains('collapsing')) {
 		
 			if (!item.classList.contains('open')) {
-				console.log('toggle accordion button');
+				console.log('toggle content button');
+
+				accCollapse.style.display = 'block';
+				let accHeight = accCollapse.clientHeight;
+				console.log(accHeight);
+
+				setTimeout(() => {
+					accCollapse.style.height = accHeight + 'px';
+					accCollapse.style.display = '';
+				}, 1);
+
+				accCollapse.classList = 'content__collapse collapsing';
+
+				setTimeout(() => {
+					console.log('open content content');
+					accCollapse.classList = 'content__collapse collapse open';
+				}, 300);
 			}
+			
+			else {
+				accCollapse.classList = 'content__collapse collapsing';
+
+				setTimeout(() => {
+					accCollapse.style.height = '0px';
+				}, 1);
+
+				setTimeout(() => {
+					console.log('close content content');
+					accCollapse.classList = 'content__collapse collapse';
+					accCollapse.style.height = '';
+				}, 300);
+			}
+
+			item.classList.toggle('open');
+		}
+	});
+});
